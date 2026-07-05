aa61799792dc05d4605af8b25405b022c7194e68
eliasalmeidaa <eliasmartinsdealmeida@gmail.com>
2026-07-05 01:27:06 -0300

    Realizado a implementação das variaveis js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,3 +1,11 @@
+const campTitulo = document.getElementById('campo-titulo');
+const campAutor= document.getElementById('campo-autor');
+const campAno = document.getElementById('campo-ano');
+const botaoAdd = document.getElementById('botao-adicionar');
+const mensFeedback = document.getElementById('mensagem-feedback');
+const contaLivros = document.getElementById('contador-livros');
+const listaLivros = document.getElementById('lista-livros');
+
 function buscarLivros() {
     fetch('http://localhost:8000/livros')
     .then(function() {
